aa38d2719f9540761563a7a3e42ac4002dcbe2fd
theprashantbhagat <bhagatprashant1997@gmail.com>
2024-01-15 13:11:35 +0530

    project

---
+++ src/main/java/com/wrapperApi/WrapperApiApplication.java
@@ -0,0 +1,13 @@
+package com.wrapperApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrapperApi/WrapperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrapperApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
